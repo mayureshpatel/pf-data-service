65d65c22353b2f091edfe56e8a698b9d0a03af4c
Mayuresh Patel <mayureshpatel1795@gmail.com>
2025-07-12 19:49:08 -0400

    Initialize `pf-data-service` project with Spring Boot, Maven Wrapper, and basic configuration files.

---
+++ src/main/java/com/mayureshpatel/pfdataservice/PfDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mayureshpatel.pfdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfDataServiceApplication.class, args);
+    }
+
+}
