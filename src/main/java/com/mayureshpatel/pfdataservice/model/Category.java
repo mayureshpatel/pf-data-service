f0f4996e2988272cb4f1127b9aa1f77782fb57c1
Mayuresh <mayureshpatel1795@gmail.com>
2025-11-29 14:35:03 -0500

    add: Introduced Category entity with JPA annotations

---
+++ src/main/java/com/mayureshpatel/pfdataservice/model/Category.java
@@ -0,0 +1,27 @@
+package com.mayureshpatel.pfdataservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String color;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
