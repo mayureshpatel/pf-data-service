fa1d25056b50b0e62993c272aee893a66263d2f9
Mayuresh <mayureshpatel1795@gmail.com>
2025-11-29 14:30:01 -0500

    add: Introduced User entity with JPA annotations

---
+++ src/main/java/com/mayureshpatel/pfdataservice/model/User.java
@@ -0,0 +1,40 @@
+package com.mayureshpatel.pfdataservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private Long lastUpdatedBy;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdatedTimestamp;
+
+    @CreationTimestamp
+    private LocalDateTime createdTimestamp;
+}
