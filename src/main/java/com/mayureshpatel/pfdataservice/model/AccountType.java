29618928de9ac2aff09840355f6510841a30a655
Mayuresh <mayureshpatel1795@gmail.com>
2025-11-29 14:24:28 -0500

    update: Introduced enums for TransactionType and AccountType

---
+++ src/main/java/com/mayureshpatel/pfdataservice/model/AccountType.java
@@ -0,0 +1,9 @@
+package com.mayureshpatel.pfdataservice.model;
+
+public enum AccountType {
+    CHECKING,
+    SAVINGS,
+    CREDIT_CARD,
+    INVESTMENT,
+    CASH
+}
---
+++ src/main/java/com/mayureshpatel/pfdataservice/model/TransactionType.java
@@ -0,0 +1,7 @@
+package com.mayureshpatel.pfdataservice.model;
+
+public enum TransactionType {
+    INCOME,
+    EXPENSE,
+    TRANSFER
+}
