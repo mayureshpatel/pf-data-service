82c8dbe85a5e276eb906fb6e26d09afe6ad1606f
Mayuresh <mayureshpatel1795@gmail.com>
2025-11-29 14:33:08 -0500

    add: Introduced Account entity with JPA annotations

---
+++ src/main/java/com/mayureshpatel/pfdataservice/model/Account.java
@@ -0,0 +1,34 @@
+package com.mayureshpatel.pfdataservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "accounts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountType type;
+
+    @Column(nullable = false)
+    private BigDecimal currentBalance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
