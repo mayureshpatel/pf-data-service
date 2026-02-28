ac4fd69652c4a87bbfbd28d5ddaadd237f1db134
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-02-27 21:20:09 -0500

    feat: add `color` field to `Tag` and remove unused `Iconography`
    - Introduce `color` field in `Tag` domain class to support tag customization.
    - Remove unused `Iconography` field, simplifying the domain model.

--- src/main/java/com/mayureshpatel/pfdataservice/domain/transaction/Tag.java
+++ src/main/java/com/mayureshpatel/pfdataservice/domain/transaction/Tag.java
@@ -1,6 +1,5 @@
 package com.mayureshpatel.pfdataservice.domain.transaction;
 
-import com.mayureshpatel.pfdataservice.domain.Iconography;
 import com.mayureshpatel.pfdataservice.domain.TableAudit;
 import com.mayureshpatel.pfdataservice.domain.user.User;
 import lombok.AllArgsConstructor;
@@ -15,7 +14,7 @@ public class Tag {
     private Long id;
     private User user;
     private String name;
+    private String color;
 
-    private Iconography iconography;
     private TableAudit audit;
 }
