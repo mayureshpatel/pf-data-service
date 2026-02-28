adcdce217ee2d1a667ab06482c8bad68ec386680
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-02-27 22:25:51 -0500

    feat: add validation constraints to `AuthenticationRequest` fields
    - Add `@NotBlank` to enforce non-empty `username` and `password`.
    - Add `@Max(50)` to restrict `username` length to 50 characters.

--- src/main/java/com/mayureshpatel/pfdataservice/dto/auth/AuthenticationRequest.java
+++ src/main/java/com/mayureshpatel/pfdataservice/dto/auth/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package com.mayureshpatel.pfdataservice.dto.auth;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 /**
@@ -10,7 +12,11 @@ import lombok.Builder;
  */
 @Builder
 public record AuthenticationRequest(
+        @NotBlank(message = "Username is required")
+        @Max(value = 50, message = "Username must be less than 50 characters")
         String username,
+
+        @NotBlank(message = "Password is required")
         String password
 ) {
 }
