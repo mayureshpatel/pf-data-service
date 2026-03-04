8f9cd3025b073b1b9a22a5d3a96204ff3851b9ae
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-03-03 19:26:31 -0500

    refactor(dto): enhance validation annotations in `AccountCreateRequest` and `AccountUpdateRequest`
    - Added field-level validation to improve data integrity, including `@Positive`, `@NotNull`, `@NotBlank`, and `@Size` constraints.
    - Updated `AccountUpdateRequest` with stricter validation rules for all fields.

--- src/main/java/com/mayureshpatel/pfdataservice/dto/account/AccountCreateRequest.java
+++ src/main/java/com/mayureshpatel/pfdataservice/dto/account/AccountCreateRequest.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 public class AccountCreateRequest {
 
     @NotNull(message = "User ID cannot be null.")
+    @Positive(message = "User ID must be a positive number.")
     private final Long userId;
 
     @NotBlank(message = "Account name cannot be blank.")
--- src/main/java/com/mayureshpatel/pfdataservice/dto/account/AccountUpdateRequest.java
+++ src/main/java/com/mayureshpatel/pfdataservice/dto/account/AccountUpdateRequest.java
@@ -1,6 +1,10 @@
 package com.mayureshpatel.pfdataservice.dto.account;
 
 import com.mayureshpatel.pfdataservice.domain.account.Account;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -10,13 +14,34 @@ import lombok.ToString;
 @ToString
 public class AccountUpdateRequest {
 
+    @NotNull(message = "Account ID cannot be null.")
+    @Positive(message = "Account ID must be a positive number.")
     private final Long id;
+
+    @NotBlank(message = "Account name cannot be blank.")
+    @Size(max = 100, message = "Account name must be less than 100 characters.")
     private final String name;
+
+    @NotBlank(message = "Account type cannot be blank.")
+    @Size(max = 20, message = "Account type must be less than 20 characters.")
     private final String type;
+
+    @NotBlank(message = "Currency code cannot be blank.")
+    @Size(min = 3, max = 3, message = "Currency code must be exactly 3 characters.")
     private final String currencyCode;
+
+    @Size(max = 50, message = "Bank name must be less than 50 characters.")
     private final String bankName;
+
+    @NotNull(message = "Version cannot be null.")
+    @Positive(message = "Version must be a positive number.")
     private final Long version;
 
+    /**
+     * Returns the domain object representation of this request.
+     *
+     * @return the domain object representation of this request.
+     */
     public Account toDomain() {
         return Account.builder()
                 .id(id)
