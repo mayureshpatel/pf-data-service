1bcf1deac56fa5ce6c19134d437207057910e461
Mayuresh Patel <mayureshpatel1795@gmail.com>
2026-03-08 15:46:17 -0400

    refactor(repository): use `MerchantDtoMapper` for cleaner `MerchantBreakdownDto` mapping
    - Replaced direct `MerchantDto` instantiation with `MerchantDtoMapper.toDto` in `MerchantTotalRowMapper`.
    - Improved code reusability and consistency by delegating `MerchantDto` creation to the mapper utility.

--- src/main/java/com/mayureshpatel/pfdataservice/repository/merchant/mapper/MerchantTotalRowMapper.java
+++ src/main/java/com/mayureshpatel/pfdataservice/repository/merchant/mapper/MerchantTotalRowMapper.java
@@ -2,6 +2,7 @@ package com.mayureshpatel.pfdataservice.repository.merchant.mapper;
 
 import com.mayureshpatel.pfdataservice.dto.merchant.MerchantBreakdownDto;
 import com.mayureshpatel.pfdataservice.dto.merchant.MerchantDto;
+import com.mayureshpatel.pfdataservice.mapper.MerchantDtoMapper;
 import com.mayureshpatel.pfdataservice.repository.JdbcMapperUtils;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,7 @@ public class MerchantTotalRowMapper extends JdbcMapperUtils implements RowMapper
 
     @Override
     public MerchantBreakdownDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-        MerchantDto merchantDto = new MerchantDto(
-                rs.getLong("merchant_id"),
-                null,
-                rs.getString("merchant_original_name"),
-                rs.getString("merchant_clean_name")
-        );
+        MerchantDto merchantDto = MerchantDtoMapper.toDto(MerchantRowMapper.mapRow(rs, "merchant"));
 
         return new MerchantBreakdownDto(merchantDto, rs.getBigDecimal("total"));
     }
